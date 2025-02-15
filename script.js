315696814058900f638c2248fcd77f11c662a574
Zeni <egor242000gg@gmail.com>
2025-02-15 09:07:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// Задержка для загрузочного экрана
+// Забавяне за зареждане на екрана
 setTimeout(function() {
     document.getElementById('loading-screen').style.display = 'none';
     document.getElementById('language-screen').style.display = 'flex';
-}, 3000); // Задержка 3 секунды
+}, 3000); 
 
-// Кнопка "Погнали" для перехода на главный экран
+// Бутон "Да вървим", за да отидете на главния екран
 document.getElementById('start-btn').addEventListener('click', function() {
-    window.location.href = 'main.html'; // Переход на новый экран
+    window.location.href = 'main.html'; 
 });
